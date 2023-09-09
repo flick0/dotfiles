20167e83293083d289a5cddcb41162a7487ceb5c
flick0 <flickerdroid211@gmail.com>
2023-09-09 15:42:55 +0000

    cleanup

--- hypr/scripts/portal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
-/usr/lib/xdg-desktop-portal &
