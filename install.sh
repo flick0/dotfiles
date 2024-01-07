20629ecd00ba63c0688f5069620e46e5ee9fa025
flicko <77581181+flick0@users.noreply.github.com>
2024-01-07 07:52:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ cd /tmp/flick0-dotfiles && cp -r ./config/* ~/.config/ && cd
 sleep 1
 echo "YorHa time!!!"
 sleep 1
-paru -S aylurs-gtk-shell-git sassc starship cava imagemagick hyprbars-git
+paru -S aylurs-gtk-shell-git sassc starship cava imagemagick hyprland-plugin-hyprbars-git
 sleep 1
 echo "Please Insert Password"
 sudo curl -L https://raw.githubusercontent.com/flick0/sttt/main/sttt -o /usr/local/bin/sttt
