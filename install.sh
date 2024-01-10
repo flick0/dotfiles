c0ae63c4f13ed5554983b8d39ce399efa6b69acf
flick0 <flickerdroid211@gmail.com>
2024-01-10 14:17:33 +0000

    poru

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ echo "done..."
 
 echo "master branch time"
 echo "installing programs the 1st"
-paru -S hyprland-git foot grim slurp swww-git fish light-git swaylock-effects-git swayidle theme.sh sddm
+paru -S hyprland-git foot grim slurp swww-git fish light-git swaylock-effects-git swayidle theme.sh sddm --noconfirm
 echo "Git Cloning from github...."
 git clone https://github.com/flick0/dotfiles /tmp/flick0-dotfiles 
 echo "copying..."
 cd /tmp/flick0-dotfiles && cp -r ./config/* ~/.config/ && cd
 echo "YorHa time!!!"
 echo "installing programs the 2nd"
-paru -S aylurs-gtk-shell-git sassc starship cava imagemagick hyprland-plugin-hyprbars-git
+paru -S aylurs-gtk-shell-git sassc starship cava imagemagick hyprland-plugin-hyprbars-git --noconfirm
 echo "Please Insert Password"
 sudo curl -L https://raw.githubusercontent.com/flick0/sttt/main/sttt -o /usr/local/bin/sttt
 sudo chmod a+rx /usr/local/bin/sttt
